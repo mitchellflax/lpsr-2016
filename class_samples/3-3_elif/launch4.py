daec10c356bc409173e22f26284ed053cc157775
Mitch Flax <mitchell.flax@gmail.com>
2016-11-17 16:28:05 +0000

    new examples of loops

--- class_samples/3-3_elif/launch4.py
+++ class_samples/3-3_elif/launch4.py
@@ -1,1 +1,10 @@
-# something about testing strings
+print('What\'s your cat\'s name?')
+cat = raw_input()
+
+if cat == "cat":
+	print('Wow, you are so creative.')
+else:
+	print('Dawwww, hi ' + cat + '!')
+
+#1: What does a user whose cat is named Fluffy see?
+#2: What's the difference between cat and "cat"?
