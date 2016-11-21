711ebeaf9ac401ee225af5acd395c6aa567fd01d
Mitch Flax <mitchell.flax@gmail.com>
2016-11-21 06:50:39 +0000

    Created launch reviewing while() loops.

---
+++ class_samples/3-6_lists/launch.py
@@ -0,0 +1,11 @@
+side = 1
+area = -1000
+
+while side <= 12:
+	area = side ** 2
+	print('The area of a square room of side ' + str(side) + ' is ' + str(area) + ' square units')
+	side = side + 1
+
+# 1) Write the first 3 lines this program outputs.
+# 2) Write the *last* line that you think this program outputs.
+# 3) What happens in lines 1 & 2, and why do you think they are set this way?
