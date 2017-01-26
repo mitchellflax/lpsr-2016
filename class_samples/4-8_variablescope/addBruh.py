3055eb217263fb2c07d21c4bdb1950e9ae7eaa3e
Mitch Flax <mitchell.flax@gmail.com>
2017-01-26 17:28:00 +0000

    Added example of variable scope misuse.

---
+++ class_samples/4-8_variablescope/addBruh.py
@@ -0,0 +1,17 @@
+def addBruh(myWords):
+	modifiedWords = 'Bruh! ' + myWords
+
+	# avoid doing this, even though it works
+	print(sentence)
+
+	return modifiedWords
+
+# test the function
+sentence = 'I want a cat!'
+newSentence = addBruh(sentence)
+
+print('The old sentence: ' + sentence)
+print('The new sentence: ' + newSentence)
+
+# this should not work! let's see...
+print(myWords)
